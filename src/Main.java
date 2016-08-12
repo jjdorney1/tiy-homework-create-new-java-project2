e9943172fc65ba5c01095186ce6f8fbe8cf9e985
jeffreydorney <jjdorney1@gmail.com>
2016-08-12 11:05:20 -0400

    corrected printed formatting and added some comments for clarity

--- src/Main.java
+++ src/Main.java
@@ -14,24 +14,25 @@ public class Main {
         soldier76.heroSet("Soldier 76", true, 30, 10, 25, 25);
         widowMaker.heroSet("Widowmaker", true, 45, 40, 30, 10);
 
-
-        System.out.println("\nLong Range Duels:");
+        // Duels ran at long range
+        System.out.println("\nLong Range Duels:\n**********************");
         System.out.println(Duel.fightTime(mcCree.heroName, mcCree.longRange, soldier76.heroName, soldier76.longRange));
         System.out.println(Duel.fightTime(mcCree.heroName, mcCree.longRange, widowMaker.heroName, widowMaker.longRange));
         System.out.println(Duel.fightTime(soldier76.heroName, soldier76.longRange, widowMaker.heroName, widowMaker.longRange));
 
-        System.out.println("\nMedium Range Duels:");
+        // Duels ran at medium range
+        System.out.println("\nMedium Range Duels: \n**********************");
         System.out.println(Duel.fightTime(mcCree.heroName, mcCree.mediumRange, soldier76.heroName, soldier76.mediumRange));
         System.out.println(Duel.fightTime(mcCree.heroName, mcCree.mediumRange, widowMaker.heroName, widowMaker.mediumRange));
         System.out.println(Duel.fightTime(soldier76.heroName, soldier76.mediumRange, widowMaker.heroName, widowMaker.mediumRange));
 
-
-        System.out.println("\nShort Range Duels:");
+        // Duels ran at short range
+        System.out.println("\nShort Range Duels: \n**********************");
         System.out.println(Duel.fightTime(mcCree.heroName, mcCree.shortRange, soldier76.heroName, soldier76.shortRange));
         System.out.println(Duel.fightTime(mcCree.heroName, mcCree.shortRange, widowMaker.heroName, widowMaker.shortRange));
         System.out.println(Duel.fightTime(soldier76.heroName, soldier76.shortRange, widowMaker.heroName, widowMaker.shortRange));
 
-//        Code to print the basic string commented out
+//        Code to print the basic string
 //        System.out.println(mcCree.heroName + " " + mcCree.ultimateReady + " " + mcCree.longRange + " " + mcCree.mediumRange + " " + mcCree.shortRange + ".");
 //        System.out.println(soldier76.heroName + " " + soldier76.ultimateReady + " " + soldier76.longRange + " " + soldier76.mediumRange + " " + soldier76.shortRange + ".");
 //        System.out.println(widowMaker.heroName + " " + widowMaker.ultimateReady + " " + widowMaker.longRange + " " + widowMaker.mediumRange + " " + widowMaker.shortRange + ".");
